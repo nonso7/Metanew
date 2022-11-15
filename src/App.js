f7742774b26a4895ec417499c69d4422628c0417
nonso7 <felixmedia78@gmail.com>
2022-11-15 13:58:57 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Section from './components/Section';
+import Place from './components/Place';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Section />} />
+          <Route path='/place' element={<Place />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
